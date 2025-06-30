c802382ef8468780fbd5b0b2daaaea1533b747e1
Matan <ml.git201@gmail.com>
2025-06-30 15:02:48 +0300

    check(auto-deploy): arbitrary commit to test render auto deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("rendered");
   return (
     <Button onClick={() => setCount((prev) => prev + 1)}>
       Click me - {count}
