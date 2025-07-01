ef6550290fb4c6f1edbca64e9e86c95c3441bdee
Matan <ml.git201@gmail.com>
2025-07-01 14:31:26 +0300

    feat(database): added db connection and prisma orm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,29 @@ import "dotenv/config";
 import { app } from "./app";
 import { env } from "./env";
 import { Server } from "http";
+import prisma from "./db";
 
 let server: Server;
 
 const init = async () => {
   const port = env.PORT || 1000;
+  await prisma.$connect();
+  console.log("Connected to db");
   server = app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
   });
 };
 
-init().catch(async (error) => {
-  console.log(error);
-});
-
-process.on("SIGINT", async () => {
+const gracefullShutdown = async () => {
   console.log("Received kill signal, shutting down gracefully");
-  server.close(() => {
-    console.log("Closed out remaining connections");
-    process.exit(0);
-  });
-});
+  await prisma.$disconnect();
+  if (server) {
+    server.close(() => {
+      console.log("Closed out remaining connections");
+      process.exit(0);
+    });
+  }
+};
+
+init().catch(gracefullShutdown);
+process.on("SIGINT", gracefullShutdown);
